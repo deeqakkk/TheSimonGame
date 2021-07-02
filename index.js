0e78cefb32dd5d37ae8d95b4009b30214ab10114
Deepak Verma <0822deepakverma@gmail.com>
2021-07-03 02:58:12 +0530

    commit

--- index.js
+++ index.js
@@ -1,43 +1,79 @@
-// Creating Random Patterns 
+
 var buttonColours = ["red", "blue", "green", "yellow"];
-//To record generated pattern
+
 var gamePattern = [];
-//To record user clicked pattern
-var userClickPattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
 
 
-//Function to create a random pattern && playing sounds also
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-    $("#" + randomChosenColour).fadeIn(100).fadeout(100).fadIn(100);
-    playSound(randomChosenColour);
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
-//Function to store user clicked pattern  & playing sounds also
-$(".btn").click(function () {
-    var userChosenColor = $(this).attr("id");
-    userClickPattern.push(userChosenColor);
-    playSound(userChosenColor);
-})
-
-
-//Function to play sound 
-function playSound(name){
-    var audio = new audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
-// Function to add pressed effect
-function animatePress(color){
-    $("#"+color).addClass("pressed");
-
-    setTimeout(function(){
-        $("#"+color).removeClass("pressed");
-    },100);
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
-
-
-
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
