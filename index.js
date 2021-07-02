28c5d846271a2eef7cb3d808f70d6b24595c0ede
Deepak Verma <0822deepakverma@gmail.com>
2021-07-03 02:32:30 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // Creating Random Patterns 
 var buttonColours = ["red", "blue", "green", "yellow"];
-
+//To record generated pattern
 var gamePattern = [];
+//To record user clicked pattern
+var userClickPattern = [];
 
+
+//Function to create a random pattern && playing sounds also
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeout(100).fadIn(100);
+    playSound(randomChosenColour);
 }
-// Selecting random selected buttons and adding properties to them
-$("#"+randomChosenColour).fadeIn(100).fadeout(100).fadIn(100);
-var audio=new audio("sounds/"+randomChosenColour+".mp3");
+
+//Function to store user clicked pattern  & playing sounds also
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr("id");
+    userClickPattern.push(userChosenColor);
+    playSound(userChosenColor);
+})
+
+
+//Function to play sound 
+function playSound(name){
+    var audio = new audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
+
+// Function to add pressed effect
+function animatePress(color){
+    $("#"+color).addClass("pressed");
+
+    setTimeout(function(){
+        $("#"+color).removeClass("pressed");
+    },100);
+}
+
+
+
+
